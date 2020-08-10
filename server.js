aee746f11be1380e0db131f842923fd86c14ca2e
Himesh Gosvami <h.gosvami@elsevier.com>
2020-08-10 12:25:55 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.get('/hello', function(req,res){
+  res.json({
+    message: 'Hello World'
+  })
+})
 app.listen(8000,function(){
   console.log("server is running")
 })
